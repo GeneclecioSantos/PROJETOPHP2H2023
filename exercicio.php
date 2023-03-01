6e4cbe5bb766ad215866c1c0a99a85f67c9d60c6
Geneclecio <geneclecio.santos.22@gmail.com>
2023-03-01 20:13:01 -0300

    first

--- exercicio.php
+++ exercicio.php
@@ -1,9 +1,8 @@
 <?php 
-
 	$valor1 = 500.00;
 	$valor2 = 600.00;
 
-  	$texto = sprintf(" O valor final é: R$ %.2f", $valor1+$valor2);
+  	$texto = sprintf(" A soma do valor é R$ %.2f", $valor1+$valor2);
 
 	echo $texto;
 
